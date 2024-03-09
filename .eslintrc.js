18f83a4955f7bbf94de0aad488622d145b0e2b5e
juwonk1018 <kjon1018@gmail.com>
2024-03-09 20:45:54 +0900

    feat: eslint, prettier, alias(craco) 추가 및 설정

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // ESLint-config-prettier 활성화(충돌하는 ESLint 규칙 무시)
+  extends: [
+    "prettier",
+    "plugin:react/recommended", // 이 줄을 extends 배열로 이동
+    "plugin:@typescript-eslint/recommended"
+  ],
+  // ESLint-plugin-prettier
+  plugins: [
+    "prettier",
+    "react", // 'plugin:' 접두사 제거 및 여기로 이동
+    "@typescript-eslint" // '@typescript-eslint' 플러그인 명시
+  ],
+  rules: {
+    // ESLint 내부에서 Prettier 실행
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        endOfLine: "auto"
+      }
+    ],
+    // 충돌이 일어나는 부분 비활성화
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+    "react/react-in-jsx-scope": "off"
+  }
+}
