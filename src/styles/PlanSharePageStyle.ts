a9ab819582e11bde9a1fb2aaf506f9c3a1556c9a
juwonk1018 <kjon1018@gmail.com>
2024-03-12 18:54:44 +0900

    style: style 위치 이동

--- src/styles/PlanSharePageStyle.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-export const SortButton = styled.div`
-  padding: 4px 16px;
-  margin-left: 40px;
-  margin-bottom: 20px;
-  border-color: transparent; // 기본 테두리 색상 설정
-`
