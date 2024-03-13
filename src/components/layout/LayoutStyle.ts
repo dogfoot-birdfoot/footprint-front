0bc35b0414dfdc24e6bfb7a5b432d3ca7fbaef5f
juwonk1018 <kjon1018@gmail.com>
2024-03-13 16:27:17 +0900

    design: 가로 1280으로 고정 및 가운데 정렬

---
+++ src/components/layout/LayoutStyle.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`
+
+export const Content = styled.main`
+  flex: 1;
+`
---
+++ src/styles/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const MyPageWrapper = styled.div`
+  width: 1280px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
