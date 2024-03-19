0dc2ff7edc054f5be4aa7a8f70b8dd43a6ea6e14
eunsol <rungjinuna@naver.com>
2024-03-19 11:48:08 +0900

    feat: add post 폼 구현

--- src/pages/ScheduleDetailPage/ScheduleDetailPage.style.ts
+++ src/pages/ScheduleDetailPage/ScheduleDetailPage.style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const ScheduleDetailStyle = styled.div`
   margin-left: 50px;
   justify-content: space-between;
-  margin-top: -270px;
+  margin-top: -500px;
 `
 export const IndexStyle = styled.div`
   background-color: var(--chakra-colors-primary);
