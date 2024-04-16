508350fdf4ecf3f8884496c37247f82898382e37
eunsol <rungjinuna@naver.com>
2024-04-16 10:36:00 +0900

    fix: memberId로인한 문제 해결

--- src/pages/CreateSchedulePage/AddPost.tsx
+++ src/pages/CreateSchedulePage/AddPost.tsx
@@ -101,7 +101,7 @@ const AddPost: React.FC = () => {
 
     console.log("Sending the following data to the server:", data) // 로그 출력
     try {
-      const response = await axios.post("https://ke4f765103c24a.user-app.krampoline.com/api/plans?memberId=5", data)
+      const response = await axios.post("https://ke4f765103c24a.user-app.krampoline.com/api/plans?memberId=4", data)
       console.log("Schedule created successfully", response.data)
       const createdAt = new Date(response.data.createdAt) // 백엔드에서 받은 'createdAt'을 Date 객체로 변환
       const formattedCreatedAt = createdAt.toISOString().split("T")[0] // 'YYYY-MM-DD' 형식으로 변환
