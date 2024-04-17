6d6da9c06f865ec8fe692b8b1fe371c859fc7b54
eunsol <rungjinuna@naver.com>
2024-04-17 12:37:56 +0900

    feat: 삭제기능추가

--- src/components/HorizontalCard/HorizontalCard.tsx
+++ src/components/HorizontalCard/HorizontalCard.tsx
@@ -5,11 +5,13 @@ import KakaoButton from "../KakaoButton/KakaoButton"
 import { TagBox, TagStyle } from "@/components/HorizontalCard/HorizontalCard.style"
 import { Avatar } from "@chakra-ui/avatar"
 import Buttons from "@/components/Buttons/Buttons"
-import { Badge, IconButton } from "@chakra-ui/react"
+import { Badge, Button, IconButton, useToast } from "@chakra-ui/react"
 import { Heading } from "@chakra-ui/react"
 import { CardInfoProps, HorizontalCardProps, ScheduleDay, ScheduleDetails } from "./type"
 import { TiStarFullOutline } from "react-icons/ti"
 import { FaRegThumbsUp } from "react-icons/fa"
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom"
 
 // 카카오톡으로 일정을 공유하는 함수
 const shareScheduleWithKakao = () => {
@@ -107,20 +109,7 @@ export const CardInfo: React.FC<CardInfoProps> = ({ ml_size, scheduleDetails }) 
     </>
   )
 }
-export const UserInfo: React.FC<{ scheduleDetails: ScheduleDetails }> = ({ scheduleDetails }) => {
-  // 'createdAt' 값을 Date 객체로 변환
-  const createdAtDate = new Date(scheduleDetails.createdAt)
-
-  // 'createdAtDate'를 'yyyy-MM-dd' 형식의 문자열로 변환
-  const formattedDate = createdAtDate
-    .toLocaleDateString("ko-KR", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit"
-    })
-    .replace(/\. /g, "-")
-    .replace(".", "")
-
+export const UserInfo: React.FC<{ scheduleDetails: ScheduleDetails }> = () => {
   return (
     <Box display="flex" justifyContent="flex-end">
       <Box mt="5">
@@ -130,7 +119,7 @@ export const UserInfo: React.FC<{ scheduleDetails: ScheduleDetails }> = ({ sched
         </Text>
         {/* 변환된 날짜 형식을 표시 */}
         <Text textAlign="end" color="gray.500" fontSize="9px" mr="5px">
-          작성일자 : {formattedDate}
+          작성일자 :
         </Text>
       </Box>
       <Avatar border="2px solid white" size="md" name="Kent Dodds" src="https://bit.ly/kent-c-dodds" mt="2" />
@@ -139,14 +128,46 @@ export const UserInfo: React.FC<{ scheduleDetails: ScheduleDetails }> = ({ sched
 }
 
 export const ScheduleButtons = () => {
+  const toast = useToast()
+  const navigate = useNavigate()
+  const { id } = useParams() // URL에서 id와 memberId 파라미터 추출
+
+  const handleDelete = async () => {
+    try {
+      // 동적으로 추출된 id를 사용하여 API 요청을 보냅니다.
+      const response = await axios.delete(`https://ke4f765103c24a.user-app.krampoline.com/api/plans/${id}`)
+      // 요청이 성공적으로 완료되면 toast 알림을 표시
+      if (response.status === 200) {
+        toast({
+          title: "여행 일정이 삭제되었습니다.",
+          description: "일정이 성공적으로 삭제되었습니다.",
+          status: "success",
+          duration: 9000,
+          isClosable: true
+        })
+        navigate("/schedule_share")
+      }
+    } catch (error) {
+      // 에러 처리
+      toast({
+        title: "오류 발생",
+        description: "일정 삭제에 실패했습니다.",
+        status: "error",
+        duration: 9000,
+        isClosable: true
+      })
+    }
+  }
   return (
     <Box>
       <Box mt="6" display="flex">
         <Box mr="2">
-          <Buttons text="수정" size="xs" />
+          <Button size="xs">수정</Button>
         </Box>
         <Box mr="2">
-          <Buttons text="삭제" size="xs" />
+          <Button size="xs" onClick={handleDelete}>
+            삭제
+          </Button>
         </Box>
         <Box mr="4">
           <Buttons text="리뷰작성" size="xs" />
