9f61ca4403ae81b39393ea731a1c0c776f727850
eunsol <rungjinuna@naver.com>
2024-04-21 16:17:50 +0900

    feat: 메인페이지 정렬 수정

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,96 +1,93 @@
 import React, { useEffect, useState } from "react"
-import axios from "axios" // API 요청을 위해 axios를 사용합니다.
+import axios from "axios"
 import { Box, Divider, Text } from "@chakra-ui/react"
-import { BannerStyle, CardListBox, Title, TitleBox, TypingText } from "./MainPage.style" // 스타일을 임포트합니다.
-import ReviewsGallery from "./ReviewsGallery" // ReviewsGallery 컴포넌트를 임포트합니다.
-import { ReviewTitle } from "./ReviewGallery.style" // 스타일을 임포트합니다.
+import { BannerStyle, CardListBox, Title, TitleBox, TypingText } from "./MainPage.style"
+import ReviewsGallery from "./ReviewsGallery"
+import { ReviewTitle } from "./ReviewGallery.style"
 import CardItem from "@/components/Card/CardItem"
 import { useNavigate } from "react-router-dom"
 import { Schedule } from "./type"
 
 const MainPage: React.FC = () => {
-  const [schedules, setSchedules] = useState<Schedule[]>([])
-  const navigate = useNavigate() // useNavigate 훅을 사용해 navigate 함수를 초기화합니다.
+  const [latestPlans, setLatestPlans] = useState<Schedule[]>([])
+  const [bestPlans, setBestPlans] = useState<Schedule[]>([])
+  const navigate = useNavigate()
 
   useEffect(() => {
-    const fetchSchedules = async () => {
+    const fetchLatestPlans = async () => {
       try {
         const response = await axios.get(
           "https://ke4f765103c24a.user-app.krampoline.com/api/plans?page=0&size=10&sort=id%2Cdesc"
         )
-        console.log("Fetched schedules:", response.data) // 추가된 부분
-        setSchedules(response.data.data.content)
+        setLatestPlans(response.data.data.content)
       } catch (error) {
-        console.error("Failed to fetch schedules:", error)
+        console.error("Failed to fetch latest plans:", error)
       }
     }
 
-    fetchSchedules()
+    const fetchBestPlans = async () => {
+      try {
+        const response = await axios.get(
+          "https://ke4f765103c24a.user-app.krampoline.com/api/plans?page=0&size=10&sort=bookmarkCount"
+        )
+        setBestPlans(response.data.data.content)
+      } catch (error) {
+        console.error("Failed to fetch best plans:", error)
+      }
+    }
+
+    fetchLatestPlans()
+    fetchBestPlans()
   }, [])
-  const handleBannerClick = () => {
-    navigate("/schedule_share") // 이동할 경로
-  }
 
   return (
     <>
-      <BannerStyle onClick={handleBannerClick}>
+      <BannerStyle onClick={() => navigate("/schedule_share")}>
         <img src="/mainImage2.jpg" alt="main image" width={"100%"} />
         <TypingText>나에게 맞는 여행 일정을 찾아보세요!</TypingText>
       </BannerStyle>
       <TitleBox>
         <Title>Latest Plans</Title>
-        <Box display="flex" justifyContent="center" mt={4}>
-          <Text mt={3} cursor="pointer" textDecoration="underline" onClick={() => navigate("/schedule_share")}>
-            View All
-          </Text>
-        </Box>
+        <Text mt={3} cursor="pointer" textDecoration="underline" onClick={() => navigate("/schedule_share")}>
+          View All
+        </Text>
       </TitleBox>
       <CardListBox>
-        {schedules.slice(0, 8).map(
-          (
-            schedule // 최대 8개의 스케줄만 표시합니다.
-          ) => (
-            <CardItem
-              key={schedule.id}
-              id={schedule.id}
-              title={schedule.title}
-              dates={`${schedule.startDate} ~ ${schedule.endDate}`}
-              bookMarkCount={schedule.bookMarkCount}
-              likeCount={schedule.likeCount}
-              author={schedule.author}
-              daySchedules={schedule.schedules}
-              createdAt={schedule.createdAt}
-            />
-          )
-        )}
+        {latestPlans.map(schedule => (
+          <CardItem
+            key={schedule.id}
+            id={schedule.id}
+            title={schedule.title}
+            dates={`${schedule.startDate} ~ ${schedule.endDate}`}
+            bookMarkCount={schedule.bookMarkCount}
+            likeCount={schedule.likeCount}
+            author={schedule.author}
+            daySchedules={schedule.schedules}
+            createdAt={schedule.createdAt}
+          />
+        ))}
       </CardListBox>
 
       <TitleBox>
         <Title>Best Plans</Title>
-        <Box display="flex" justifyContent="center" mt={4}>
-          <Text mt={3} cursor="pointer" textDecoration="underline" onClick={() => navigate("/schedule_share")}>
-            View All
-          </Text>
-        </Box>
+        <Text mt={3} cursor="pointer" textDecoration="underline" onClick={() => navigate("/schedule_share")}>
+          View All
+        </Text>
       </TitleBox>
       <CardListBox>
-        {schedules.slice(0, 8).map(
-          (
-            schedule // 최대 8개의 스케줄만 표시합니다.
-          ) => (
-            <CardItem
-              key={schedule.id}
-              id={schedule.id}
-              title={schedule.title}
-              dates={`${schedule.startDate} ~ ${schedule.endDate}`}
-              bookMarkCount={schedule.bookMarkCount}
-              likeCount={schedule.likeCount}
-              author={schedule.author}
-              daySchedules={schedule.schedules}
-              createdAt={schedule.createdAt}
-            />
-          )
-        )}
+        {bestPlans.map(schedule => (
+          <CardItem
+            key={schedule.id}
+            id={schedule.id}
+            title={schedule.title}
+            dates={`${schedule.startDate} ~ ${schedule.endDate}`}
+            bookMarkCount={schedule.bookMarkCount}
+            likeCount={schedule.likeCount}
+            author={schedule.author}
+            daySchedules={schedule.schedules}
+            createdAt={schedule.createdAt}
+          />
+        ))}
       </CardListBox>
 
       <Box display="flex" flexDirection="column" alignItems="center" mt={20} mb={20}>
